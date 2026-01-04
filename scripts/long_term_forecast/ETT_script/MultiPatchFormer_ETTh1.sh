583e4b6c311fdd487ef5285767ca6c6eb2c6a04b
jungletada <1002955176@qq.com>
2026-01-04 22:04:33 +0900

    ETT

--- scripts/long_term_forecast/ETT_script/MultiPatchFormer_ETTh1.sh
+++ scripts/long_term_forecast/ETT_script/MultiPatchFormer_ETTh1.sh
@@ -1,85 +1,83 @@
 export CUDA_VISIBLE_DEVICES=0
 
-model_name=MultiPatchFormer
-dataset=ETTh1
+pred_lens=(96 192 336 720)
+for i in "${!pred_lens[@]}"; do
+  python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1 \
+    --data_name ETTh1 \
+    --model_id ETTh1_96_${pred_lens[$i]} \
+    --model MultiPatchFormer \
+    --features S \
+    --target OT \
+    --enc_in 1 \
+    --dec_in 1 \
+    --c_out 1 \
+    --seq_len 96 \
+    --label_len 48 \
+    --pred_len ${pred_lens[$i]} \
+    --e_layers 1 \
+    --d_model 256 \
+    --d_ff 512 \
+    --des 'Exp' \
+    --itr 1
+done
+
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --data_name $dataset \
+#   --model_id ETTh1_96_192 \
+#   --model $model_name \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 192 \
+#   --e_layers 1 \
+#   --enc_in 7 \
+#   --dec_in 7 \
+#   --c_out 7 \
+#   --d_model 256 \
+#   --d_ff 512 \
+#   --des 'Exp' \
+#   --itr 1
 
 
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --data_name $dataset \
-  --model_id ETTh1_96_96 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 96 \
-  --e_layers 1 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --d_model 256 \
-  --d_ff 512 \
-  --des 'Exp' \
-  --itr 1
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --data_name $dataset \
+#   --model_id ETTh1_96_336 \
+#   --model $model_name \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 336 \
+#   --e_layers 1 \
+#   --enc_in 7 \
+#   --dec_in 7 \
+#   --c_out 7 \
+#   --d_model 256 \
+#   --d_ff 512 \
+#   --des 'Exp' \
+#   --itr 1
 
 
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --data_name $dataset \
-  --model_id ETTh1_96_192 \
-  --model $model_name \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 192 \
-  --e_layers 1 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --d_model 256 \
-  --d_ff 512 \
-  --des 'Exp' \
-  --itr 1
-
-
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --data_name $dataset \
-  --model_id ETTh1_96_336 \
-  --model $model_name \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 336 \
-  --e_layers 1 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --d_model 256 \
-  --d_ff 512 \
-  --des 'Exp' \
-  --itr 1
-
-
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --data_name $dataset \
-  --model_id ETTh1_96_720 \
-  --model $model_name \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 720 \
-  --e_layers 1 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --d_model 256 \
-  --d_ff 512 \
-  --des 'Exp' \
-  --itr 1
+# python -u run.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --data_name $dataset \
+#   --model_id ETTh1_96_720 \
+#   --model $model_name \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 720 \
+#   --e_layers 1 \
+#   --enc_in 7 \
+#   --dec_in 7 \
+#   --c_out 7 \
+#   --d_model 256 \
+#   --d_ff 512 \
+#   --des 'Exp' \
+#   --itr 1
---
+++ scripts/long_term_forecast/ETT_script/PAttn_ETTm1.sh
@@ -0,0 +1,22 @@
+export CUDA_VISIBLE_DEVICES=0
+
+pred_lens=(96 192 336 720)
+for i in "${!pred_lens[@]}"; do
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --data_name ETTm1 \
+  --model_id ETTm1_96_${pred_lens[$i]} \
+  --model PAttn \
+  --features S \
+  --target OT \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len ${pred_lens[$i]} \
+  --factor 3 \
+  --des 'Exp' \
+  --itr 1
+done
