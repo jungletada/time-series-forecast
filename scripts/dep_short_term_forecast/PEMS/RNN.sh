6ab9ec430fc7cea6694ae8a792c43d86804fd641
jungletada <1002955176@qq.com>
2026-01-18 18:03:22 +0900

    test

--- scripts/dep_short_term_forecast/PEMS/RNN.sh
+++ scripts/dep_short_term_forecast/PEMS/RNN.sh
@@ -5,52 +5,147 @@ e_layers=3
 dropout=0.1
 features=S
 pred_lens=(12 24 48)
-datasets=(PEMS03_dep PEMS04_dep PEMS07_dep PEMS08_dep)
+# datasets=(PEMS03_dep PEMS04_dep PEMS07_dep PEMS08_dep)
 
-for dataset in "${datasets[@]}"; do
-    for pred_len in "${pred_lens[@]}"; do
-        python -u run_dep.py \
-            --task_name long_term_forecast \
-            --seed 5566 \
-            --is_training 1 \
-            --data_name $dataset \
-            --model $model_name \
-            --features $features \
-            --seq_len 96 \
-            --label_len 0 \
-            --pred_len $pred_len \
-            --d_model $d_model \
-            --rnn_type $rnn_type \
-            --e_layers $e_layers \
-            --dropout $dropout \
-            --des 'Exp' \
-            --batch_size 32 \
-            --learning_rate 0.001 \
-            --train_epochs 10 \
-            --patience 5 \
-            --itr 1
+dataset=PEMS03_dep
+for pred_len in "${pred_lens[@]}"; do
+    python -u run_dep.py \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --data_name $dataset \
+        --model $model_name \
+        --features $features \
+        --seq_len 96 \
+        --label_len 0 \
+        --pred_len $pred_len \
+        --rnn_type $rnn_type \
+        --des 'Exp' \
+        --batch_size 32 \
+        --learning_rate 0.001 \
+        --train_epochs 10 \
+        --patience 5 \
+        --itr 1
 
-        python -u run_dep.py \
-            --task_name long_term_forecast \
-            --seed 5566 \
-            --is_training 0 \
-            --use_mnn 1 \
-            --mnn mlp \
-            --data_name $dataset \
-            --model $model_name \
-            --features $features \
-            --seq_len 96 \
-            --label_len 0 \
-            --pred_len $pred_len \
-            --d_model $d_model \
-            --rnn_type $rnn_type \
-            --e_layers $e_layers \
-            --dropout $dropout \
-            --des 'Exp' \
-            --batch_size 32 \
-            --learning_rate 0.001 \
-            --train_epochs 10 \
-            --patience 5 \
-            --itr 1
-    done
+    python -u run_dep.py \
+        --task_name long_term_forecast \
+        --is_training 0 \
+        --use_mnn 1 \
+        --data_name $dataset \
+        --model $model_name \
+        --features $features \
+        --seq_len 96 \
+        --label_len 0 \
+        --pred_len $pred_len \
+        --rnn_type $rnn_type \
+        --des 'Exp' \
+        --batch_size 16 \
+        --itr 1
 done
+
+dataset=PEMS04_dep
+for pred_len in "${pred_lens[@]}"; do
+    python -u run_dep.py \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --data_name $dataset \
+        --model $model_name \
+        --features $features \
+        --seq_len 96 \
+        --label_len 0 \
+        --pred_len $pred_len \
+        --rnn_type $rnn_type \
+        --des 'Exp' \
+        --batch_size 32 \
+        --learning_rate 0.001 \
+        --train_epochs 10 \
+        --patience 5 \
+        --itr 1
+
+    python -u run_dep.py \
+        --task_name long_term_forecast \
+        --is_training 0 \
+        --use_mnn 1 \
+        --data_name $dataset \
+        --model $model_name \
+        --features $features \
+        --seq_len 96 \
+        --label_len 0 \
+        --pred_len $pred_len \
+        --rnn_type $rnn_type \
+        --des 'Exp' \
+        --batch_size 16 \
+        --itr 1
+done
+
+
+dataset=PEMS07_dep
+for pred_len in "${pred_lens[@]}"; do
+    python -u run_dep.py \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --data_name $dataset \
+        --model $model_name \
+        --features $features \
+        --seq_len 96 \
+        --label_len 0 \
+        --pred_len $pred_len \
+        --rnn_type $rnn_type \
+        --des 'Exp' \
+        --batch_size 32 \
+        --learning_rate 0.001 \
+        --train_epochs 10 \
+        --patience 5 \
+        --itr 1
+
+    python -u run_dep.py \
+        --task_name long_term_forecast \
+        --is_training 0 \
+        --use_mnn 1 \
+        --data_name $dataset \
+        --model $model_name \
+        --features $features \
+        --seq_len 96 \
+        --label_len 0 \
+        --pred_len $pred_len \
+        --rnn_type $rnn_type \
+        --des 'Exp' \
+        --batch_size 16 \
+        --itr 1
+done
+
+
+dataset=PEMS08_dep
+for pred_len in "${pred_lens[@]}"; do
+    python -u run_dep.py \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --data_name $dataset \
+        --model $model_name \
+        --features $features \
+        --seq_len 96 \
+        --label_len 0 \
+        --pred_len $pred_len \
+        --rnn_type $rnn_type \
+        --des 'Exp' \
+        --batch_size 32 \
+        --learning_rate 0.001 \
+        --train_epochs 10 \
+        --patience 5 \
+        --itr 1
+
+    python -u run_dep.py \
+        --task_name long_term_forecast \
+        --is_training 0 \
+        --use_mnn 1 \
+        --data_name $dataset \
+        --model $model_name \
+        --features $features \
+        --seq_len 96 \
+        --label_len 0 \
+        --pred_len $pred_len \
+        --rnn_type $rnn_type \
+        --des 'Exp' \
+        --batch_size 16 \
+        --itr 1
+done
+
--- scripts/dep_short_term_forecast/PEMS/TCN.sh
+++ scripts/dep_short_term_forecast/PEMS/TCN.sh
@@ -4,10 +4,10 @@ d_model=36
 e_layers=4
 features=S
 pred_lens=(12 24 48)
-datasets=(PEMS03_dep PEMS04_dep PEMS07_dep PEMS08_dep)
 
-for dataset in "${datasets[@]}"; do
-    for pred_len in "${pred_lens[@]}"; do
+
+dataset=PEMS03_dep
+for pred_len in "${pred_lens[@]}"; do
     python run_dep.py \
         --task_name long_term_forecast \
         --is_training 1 \
@@ -18,7 +18,7 @@ for dataset in "${datasets[@]}"; do
         --seq_len 96 \
         --pred_len $pred_len \
         --des 'Exp' \
-        --batch_size 16 \
+        --batch_size 32 \
         --train_epochs 10 \
         --patience 5 \
         --itr 1
@@ -36,5 +36,133 @@ for dataset in "${datasets[@]}"; do
         --des 'Exp' \
         --batch_size 16 \
         --itr 1
-    done
 done
+
+
+# dataset=PEMS03_dep
+# for pred_len in "${pred_lens[@]}"; do
+#     python run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 1 \
+#         --data_name $dataset \
+#         --model $model_name \
+#         --model_id $model_name \
+#         --features $features \
+#         --seq_len 96 \
+#         --pred_len $pred_len \
+#         --des 'Exp' \
+#         --batch_size 16 \
+#         --train_epochs 10 \
+#         --patience 5 \
+#         --itr 1
+
+#     python run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 0 \
+#         --use_mnn 1 \
+#         --data_name $dataset \
+#         --model $model_name \
+#         --model_id $model_name \
+#         --features $features \
+#         --seq_len 96 \
+#         --pred_len $pred_len \
+#         --des 'Exp' \
+#         --batch_size 16 \
+#         --itr 1
+# done
+
+# dataset=PEMS04_dep
+# for pred_len in "${pred_lens[@]}"; do
+#     python run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 1 \
+#         --data_name $dataset \
+#         --model $model_name \
+#         --model_id $model_name \
+#         --features $features \
+#         --seq_len 96 \
+#         --pred_len $pred_len \
+#         --des 'Exp' \
+#         --batch_size 16 \
+#         --train_epochs 10 \
+#         --patience 5 \
+#         --itr 1
+
+#     python run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 0 \
+#         --use_mnn 1 \
+#         --data_name $dataset \
+#         --model $model_name \
+#         --model_id $model_name \
+#         --features $features \
+#         --seq_len 96 \
+#         --pred_len $pred_len \
+#         --des 'Exp' \
+#         --batch_size 16 \
+#         --itr 1
+# done
+
+# dataset=PEMS07_dep
+# for pred_len in "${pred_lens[@]}"; do
+#     python run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 1 \
+#         --data_name $dataset \
+#         --model $model_name \
+#         --model_id $model_name \
+#         --features $features \
+#         --seq_len 96 \
+#         --pred_len $pred_len \
+#         --des 'Exp' \
+#         --batch_size 16 \
+#         --train_epochs 10 \
+#         --patience 5 \
+#         --itr 1
+
+#     python run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 0 \
+#         --use_mnn 1 \
+#         --data_name $dataset \
+#         --model $model_name \
+#         --model_id $model_name \
+#         --features $features \
+#         --seq_len 96 \
+#         --pred_len $pred_len \
+#         --des 'Exp' \
+#         --batch_size 16 \
+#         --itr 1
+# done
+
+# dataset=PEMS08_dep
+# for pred_len in "${pred_lens[@]}"; do
+#     python run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 1 \
+#         --data_name $dataset \
+#         --model $model_name \
+#         --model_id $model_name \
+#         --features $features \
+#         --seq_len 96 \
+#         --pred_len $pred_len \
+#         --des 'Exp' \
+#         --batch_size 16 \
+#         --train_epochs 10 \
+#         --patience 5 \
+#         --itr 1
+
+#     python run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 0 \
+#         --use_mnn 1 \
+#         --data_name $dataset \
+#         --model $model_name \
+#         --model_id $model_name \
+#         --features $features \
+#         --seq_len 96 \
+#         --pred_len $pred_len \
+#         --des 'Exp' \
+#         --batch_size 16 \
+#         --itr 1
+# done
