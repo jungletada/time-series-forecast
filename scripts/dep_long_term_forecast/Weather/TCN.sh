c603e311dac1076d546f648398555e4ec1efed7d
jungletada <1002955176@qq.com>
2026-01-19 22:01:15 +0900

    test

--- scripts/dep_long_term_forecast/Weather/TCN.sh
+++ scripts/dep_long_term_forecast/Weather/TCN.sh
@@ -1,33 +1,29 @@
-export CUDA_VISIBLE_DEVICES=0
 d_model=32
 e_layers=4
 model_id=NDA+TCN
-pred_lens=(96 192 336 720)
+pred_lens=(720)
 
-for i in "${!pred_lens[@]}"; do
-    python -u run_dep.py \
-        --task_name long_term_forecast \
-        --is_training 1 \
-        --use_mnn 0 \
-        --data_name Weather_dep \
-        --model_id $model_id \
-        --model TCN \
-        --seq_len 96 \
-        --label_len 0 \
-        --pred_len ${pred_lens[$i]} \
-        --enc_in 1 \
-        --c_out 1 \
-        --target OT \
-        --features S \
-        --d_model $d_model \
-        --e_layers $e_layers \
-        --des 'Exp' \
-        --itr 1 \
-        --learning_rate 0.01 \
-        --train_epochs 10 \
-        --patience 10 \
-        --batch_size 128
-done
+model_configs=(configs/models/weather/TCN_0.yaml configs/models/weather/TCN_1.yaml configs/models/weather/TCN_2.yaml)
+# for i in "${!pred_lens[@]}"; do
+#     python -u run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 1 \
+#         --use_mnn 0 \
+#         --data_name Weather_dep \
+#         --model_id $model_id \
+#         --model_configs ${model_configs[@]} \
+#         --model TCN \
+#         --features S \
+#         --seq_len 96 \
+#         --label_len 0 \
+#         --pred_len ${pred_lens[$i]} \
+#         --des 'Exp' \
+#         --itr 1 \
+#         --learning_rate 0.01 \
+#         --train_epochs 10 \
+#         --patience 10 \
+#         --batch_size 128
+# done
 
 
 for i in "${!pred_lens[@]}"; do
@@ -37,16 +33,12 @@ for i in "${!pred_lens[@]}"; do
         --use_mnn 1 \
         --data_name Weather_dep \
         --model_id $model_id \
+        --model_configs ${model_configs[@]} \
         --model TCN \
         --seq_len 96 \
         --label_len 0 \
         --pred_len ${pred_lens[$i]} \
-        --enc_in 1 \
-        --c_out 1 \
-        --target OT \
         --features S \
-        --d_model $d_model \
-        --e_layers $e_layers \
         --des 'Exp' \
         --itr 1 
 done
--- scripts/dep_short_term_forecast/PEMS/TCN.sh
+++ scripts/dep_short_term_forecast/PEMS/TCN.sh
@@ -4,50 +4,52 @@ d_model=36
 e_layers=4
 features=S
 
-pred_lens=(12 24 48)
-# dataset=PEMS03_dep
-# model_configs=(configs/models/PEMS03/TCN_0.yaml configs/models/PEMS03/TCN_1.yaml configs/models/PEMS03/TCN_2.yaml)
-# for pred_len in "${pred_lens[@]}"; do
-#     python run_dep.py \
-#         --task_name long_term_forecast \
-#         --is_training 1 \
-#         --data_name $dataset \
-#         --model_configs ${model_configs[@]} \
-#         --model $model_name \
-#         --model_id $model_name \
-#         --features $features \
-#         --seq_len 96 \
-#         --pred_len $pred_len \
-#         --des 'Exp' \
-#         --batch_size 16 \
-#         --train_epochs 20 \
-#         --patience 10 \
-#         --itr 1
+pred_lens=(48)
+dataset=PEMS03_dep
+model_configs=(configs/models/PEMS03/TCN_0.yaml configs/models/PEMS03/TCN_1.yaml configs/models/PEMS03/TCN_2.yaml)
+for pred_len in "${pred_lens[@]}"; do
+    python run_dep.py \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --data_name $dataset \
+        --model_configs ${model_configs[@]} \
+        --model $model_name \
+        --model_id $model_name \
+        --features $features \
+        --seq_len 96 \
+        --pred_len $pred_len \
+        --des 'Exp' \
+        --batch_size 16 \
+        --train_epochs 20 \
+        --patience 10 \
+        --itr 1
 
-#     python run_dep.py \
-#         --task_name long_term_forecast \
-#         --is_training 0 \
-#         --use_mnn 1 \
-#         --data_name $dataset \
-#         --model_configs ${model_configs[@]} \
-#         --model $model_name \
-#         --model_id $model_name \
-#         --features $features \
-#         --seq_len 96 \
-#         --pred_len $pred_len \
-#         --des 'Exp' \
-#         --batch_size 16 \
-#         --itr 1
-# done
+    # python run_dep.py \
+    #     --task_name long_term_forecast \
+    #     --is_training 0 \
+    #     --use_mnn 1 \
+    #     --data_name $dataset \
+    #     --model_configs ${model_configs[@]} \
+    #     --model $model_name \
+    #     --model_id $model_name \
+    #     --features $features \
+    #     --seq_len 96 \
+    #     --pred_len $pred_len \
+    #     --des 'Exp' \
+    #     --batch_size 16 \
+    #     --itr 1
+done
 
 
-# pred_lens=(12 24 48)
+# pred_lens=(12)
 # dataset=PEMS04_dep
+# model_configs=(configs/models/PEMS04/TCN_0.yaml configs/models/PEMS04/TCN_1.yaml configs/models/PEMS04/TCN_2.yaml)
 # for pred_len in "${pred_lens[@]}"; do
 #     python run_dep.py \
 #         --task_name long_term_forecast \
 #         --is_training 1 \
 #         --data_name $dataset \
+#         --model_configs ${model_configs[@]} \
 #         --model $model_name \
 #         --model_id $model_name \
 #         --features $features \
@@ -64,6 +66,7 @@ pred_lens=(12 24 48)
 #         --is_training 0 \
 #         --use_mnn 1 \
 #         --data_name $dataset \
+#         --model_configs ${model_configs[@]} \
 #         --model $model_name \
 #         --model_id $model_name \
 #         --features $features \
@@ -74,9 +77,45 @@ pred_lens=(12 24 48)
 #         --itr 1
 # done
 
-pred_lens=(24)
-dataset=PEMS07_dep
-model_configs=(configs/models/PEMS07/TCN_0.yaml configs/models/PEMS07/TCN_1.yaml configs/models/PEMS07/TCN_2.yaml)
+# pred_lens=(12)
+# dataset=PEMS07_dep
+# model_configs=(configs/models/PEMS07/TCN_0.yaml configs/models/PEMS07/TCN_1.yaml configs/models/PEMS07/TCN_2.yaml)
+# for pred_len in "${pred_lens[@]}"; do
+#     python run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 1 \
+#         --data_name $dataset \
+#         --model_configs ${model_configs[@]} \
+#         --model $model_name \
+#         --model_id $model_name \
+#         --features $features \
+#         --seq_len 96 \
+#         --pred_len $pred_len \
+#         --des 'Exp' \
+#         --batch_size 16 \
+#         --train_epochs 10 \
+#         --patience 5 \
+#         --itr 1
+
+#     python run_dep.py \
+#         --task_name long_term_forecast \
+#         --is_training 0 \
+#         --use_mnn 1 \
+#         --data_name $dataset \
+#         --model_configs ${model_configs[@]} \
+#         --model $model_name \
+#         --model_id $model_name \
+#         --features $features \
+#         --seq_len 96 \
+#         --pred_len $pred_len \
+#         --des 'Exp' \
+#         --batch_size 16 \
+#         --itr 1
+# done
+
+pred_lens=(12)
+dataset=PEMS08_dep
+model_configs=(configs/models/PEMS08/TCN_0.yaml configs/models/PEMS08/TCN_1.yaml configs/models/PEMS08/TCN_2.yaml)
 for pred_len in "${pred_lens[@]}"; do
     # python run_dep.py \
     #     --task_name long_term_forecast \
@@ -94,44 +133,12 @@ for pred_len in "${pred_lens[@]}"; do
     #     --patience 5 \
     #     --itr 1
 
-    python run_dep.py \
-        --task_name long_term_forecast \
-        --is_training 0 \
-        --use_mnn 1 \
-        --data_name $dataset \
-        --model_configs ${model_configs[@]} \
-        --model $model_name \
-        --model_id $model_name \
-        --features $features \
-        --seq_len 96 \
-        --pred_len $pred_len \
-        --des 'Exp' \
-        --batch_size 16 \
-        --itr 1
-done
-
-# dataset=PEMS08_dep
-# for pred_len in "${pred_lens[@]}"; do
-#     python run_dep.py \
-#         --task_name long_term_forecast \
-#         --is_training 1 \
-#         --data_name $dataset \
-#         --model $model_name \
-#         --model_id $model_name \
-#         --features $features \
-#         --seq_len 96 \
-#         --pred_len $pred_len \
-#         --des 'Exp' \
-#         --batch_size 16 \
-#         --train_epochs 10 \
-#         --patience 5 \
-#         --itr 1
-
 #     python run_dep.py \
 #         --task_name long_term_forecast \
 #         --is_training 0 \
 #         --use_mnn 1 \
 #         --data_name $dataset \
+#         --model_configs ${model_configs[@]} \
 #         --model $model_name \
 #         --model_id $model_name \
 #         --features $features \
