252934ef5e13ceec028a991fb360e172fdb4102b
jungletada <1002955176@qq.com>
2026-02-05 18:13:22 +0900

    update

--- scripts/dep_long_term_forecast/ETTh1/TimeFilter.sh
+++ scripts/dep_long_term_forecast/ETTh1/TimeFilter.sh
@@ -2,28 +2,13 @@ dataset=ETTh1_dep
 model_name=TimeFilter
 model_configs=(configs/models/ETTh1/Timefilter_0.yaml configs/models/ETTh1/Timefilter_1.yaml configs/models/ETTh1/Timefilter_2.yaml)
 pred_lens=(96 )
-selected_k=3 # 选择的分解主元
+selected_k=2    # 选择的分解主元
 
-# for i in 2 1 0; do
-# python -u tune.py \
-#     --is_training 1 \
-#     --task_name long_term_forecast \
-#     --model $model_name \
-#     --model_configs ${model_configs[@]} \
-#     --data_name $dataset \
-#     --features M \
-#     --seq_len 96 \
-#     --label_len 48 \
-#     --pred_len 96 \
-#     --selected_k ${selected_k} \
-#     --train_component ${i} \
-#     --des 'Tuning_Exp' \
-#     --itr 1 
-# done
-
-python -u run_dep.py \
+for i in 2 1 0; do
+python -u tune.py \
     --is_training 1 \
     --task_name long_term_forecast \
+    --model $model_name \
     --model_configs ${model_configs[@]} \
     --data_name $dataset \
     --features M \
@@ -31,5 +16,20 @@ python -u run_dep.py \
     --label_len 48 \
     --pred_len 96 \
     --selected_k ${selected_k} \
-    --des 'Exp' \
-    --itr 1
+    --train_component ${i} \
+    --des 'Tuning_Exp' \
+    --itr 1 
+done
+
+# python -u run_dep.py \
+#     --is_training 1 \
+#     --task_name long_term_forecast \
+#     --model_configs ${model_configs[@]} \
+#     --data_name $dataset \
+#     --features M \
+#     --seq_len 96 \
+#     --label_len 48 \
+#     --pred_len 96 \
+#     --selected_k ${selected_k} \
+#     --des 'Exp' \
+#     --itr 1
