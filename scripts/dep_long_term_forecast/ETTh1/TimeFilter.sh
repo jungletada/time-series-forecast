90d5f07df7cdbcdd78bf3c7bcfa857aba7a07e59
jungletada <1002955176@qq.com>
2026-02-08 00:26:40 +0900

    update

--- scripts/dep_long_term_forecast/ETTh1/TimeFilter.sh
+++ scripts/dep_long_term_forecast/ETTh1/TimeFilter.sh
@@ -6,11 +6,9 @@ model_configs=(
     configs/models/ETTh1/Timefilter_2.yaml)
     
 pred_lens=(96 192 336 720)
-best_pivot=(3 4 4 4)
+best_pivot=(4 4 4 4)
 
-pivot=4
-for i in "${!pred_lens[@]}"; do
-    python -u run_dep.py \
+python -u run_dep.py \
     --is_training 1 \
     --task_name long_term_forecast \
     --model $model_name \
@@ -18,14 +16,14 @@ for i in "${!pred_lens[@]}"; do
     --data_name $dataset \
     --features M \
     --seq_len 96 \
+    
     --label_len 48 \
-    --pred_len ${pred_lens[$i]} \
-    --pivot $pivot \
+    --pred_len 192 \
+    --pivot 4 \
     --des 'Exp' \
     --itr 1
-done
 
-# pivot=3    # 选择的分解主元
+# pivot=4    # 选择的分解主元
 # for i in 2 1 0; do
 # python -u tune.py \
 #     --is_training 1 \
@@ -36,7 +34,7 @@ done
 #     --features M \
 #     --seq_len 96 \
 #     --label_len 48 \
-#     --pred_len 96 \
+#     --pred_len 192 \
 #     --pivot ${pivot} \
 #     --train_component ${i} \
 #     --des 'Tuning_Exp' \
